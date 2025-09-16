c178416bf89efc27dfdf04ac89d8be9e71aacfaf
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-09-16 16:53:31 -0600

    Agregar configuración inicial y estructura básica para la aplicación de tareas en React. Se incluyen archivos como .eslintrc.cjs, .gitignore, index.html, package.json, package-lock.json, y componentes como App.jsx, TaskList.jsx, y TaskItem.jsx, junto con estilos CSS y lógica para la gestión de tareas.

---
+++ projects/todo-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main>
+      <h1>Todo List App</h1>
+
+      <form action="">
+        <input type="text" />
+        <button></button>
+      </form>
+    </main>
+  );
+}
+
+export default App
